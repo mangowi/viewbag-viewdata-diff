6680b1a7b15fa9452e799f2b40efed7b3c3183c5
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-16 02:02:11 +0200

    Add project files.

---
+++ ViewBagViewData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViewBagViewData.Startup))]
+namespace ViewBagViewData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
